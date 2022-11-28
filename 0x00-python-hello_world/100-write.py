eec69282ade5b0d0e7062a823d5c49d2eb86a262
Donald <donumeh54@gmail.com>
2022-11-28 23:05:41 +0100

    exit mode to return non-zero

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,9 +2,9 @@
 import sys
 
 
-def stringg(*a):
-    print(*a, file=sys.stderr)
-    sys.exit(1)
+var = sys.stderr
+stringg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 
-
-stringg("and that piece of art is useful - Dora Korpar, 2015-10-19")
+var.write(stringg)
+var.write('\n')
+sys.exit(1)
