536a49e06db50f24084845c66aeb9257005bee6c
Donald <donumeh54@gmail.com>
2022-11-29 19:13:27 +0100

    print a-z in lowercase withour q or e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,6 +3,6 @@
 n = 26
 
 for i in range(97, 97 + n):
-    if (chr(i) == 'q') or (chr(i) == 'e'):
-        continue;
+    if (chr(i) == "q") or (chr(i) == "e"):
+        continue
     print("{}".format(chr(i)), end="")
