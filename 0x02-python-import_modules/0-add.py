2e36e4db87c111e9ad3c316e52608ba6ea42e627
Donald <donumeh54@gmail.com>
2022-12-05 00:09:52 +0100

    import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
+
+add = __import__('add_0').add
 
 a = 1
 b = 2
-
 n = add(a, b)
 
-print("{} + {} = {}".format(a, b, n))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, n))
 
