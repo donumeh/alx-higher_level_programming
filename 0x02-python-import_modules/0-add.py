4042557f8e6e2535ae29f37bc629fc1834f70572
Donald <donumeh54@gmail.com>
2023-01-01 21:15:03 +0100

    adds two int

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-add = __import__("add_0").add
-
-a = 1
-b = 2
-
 if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
     n = add(a, b)
-    print("{} + {} = {}".format(a, b, n))
+    print("{:d} + {:d} = {:d}".format(a, b, n))
