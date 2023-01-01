878515de1c7c569cebb440d302cbc4299fa583a0
Donald <donumeh54@gmail.com>
2023-01-01 21:57:53 +0100

    copies args of a string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) > 1:
-        print("{:d} arguments:".format(len(argv) - 1))
+        if (len(argv) - 1) == 1:
+            print("{:d} argument:".format(len(argv) - 1))
+        elif (len(argv) - 1) > 1:
+            print("{:d} arguments:".format(len(argv) - 1))
         n = 0
         for i in argv[1:]:
             n = n + 1
