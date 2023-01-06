68e70dcd108069e50d78ad6de34a447835e8bb67
Donald <donumeh54@gmail.com>
2023-01-06 17:12:03 +0100

    replace a value in a list using lambda

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 
 
 def search_replace(my_list, search, replace):
-    return [list(map(lambda x: replace if x == search else x, my_list))]
+    return (list(map(lambda x: replace if x == search else x, my_list)))
