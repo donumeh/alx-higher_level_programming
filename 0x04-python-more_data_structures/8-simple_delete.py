588b885f423abf20bacbbc25f708bf223d3df888
Donald <donumeh54@gmail.com>
2023-01-11 10:32:22 +0100

    simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def simple_delete(a_dictionary, key=""):
     for i, v in a_dictionary.items():
         if i == key:
-           del a_dictionary[key]
-           break
-    return (a_dictionary)
+            del a_dictionary[key]
+            break
+    return a_dictionary
