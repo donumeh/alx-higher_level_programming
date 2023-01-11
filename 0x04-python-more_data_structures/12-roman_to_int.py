53c4ff1016a4638b960fa075686a80229b9c93a0
Donald <donumeh54@gmail.com>
2023-01-11 11:32:58 +0100

    romans to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,66 +1,18 @@
 #!/usr/bin/python3
-
-def check_roman(x):
-    if x == "I":
-        return 1
-    if x == "IV":
-        return 4
-    if x == "V":
-        return 5
-    if x == "IX":
-        return 9
-    if x == "X":
-        return 10
-    if x == "XL":
-        return 40
-    if x == "L":
-        return 50
-    if x == "XC":
-        return 90
-    if x == "C":
-        return 100
-    if x == "CD":
-        return 400
-    if x == "D":
-        return 500
-    if x == "CM":
-        return 900
-    if x == "M":
-        return 1000
-
-
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
-    sumi = 0
+    roman_numeral = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    result = 0
     for i in range(len(roman_string)):
-        if (roman_string[i] == "I") and (i != (len(roman_string) - 1)):
-            if roman_string[i + 1] == "V":
-                i = i + 1
-                sumi = sumi + check_roman("IV")
-            elif roman_string[i + 1] == "X":
-                i = i + 1
-                sumi = sumi + check_roman("IX")
-            else:
-                sumi = sumi + check_roman(roman_string[i])
-        elif roman_string[i] == "X" and (i != (len(roman_string) - 1)):
-            if roman_string[i + 1] == "L":
-                i = i + 1
-                sumi = sumi + check_roman("XL")
-            elif roman_string[i + 1] == "C":
-                i = i + 1
-                sumi = sumi + check_roman("XC")
-            else:
-                sumi = sumi + check_roman(roman_string[i])
-        elif roman_string[i] == "C" and (i != (len(roman_string) - 1)):
-            if roman_string[i + 1] == "D":
-                i = i + 1
-                sumi = sumi + check_roman("CD")
-            elif roman_string[i + 1] == "M":
-                i = i + 1
-                sumi = sumi + check_roman("CM")
-            else:
-                sumi = sumi + check_roman(roman_string[i])
+        if i > 0 and roman_numeral[roman_string[i]] > roman_numeral[roman_string[i - 1]]:
+            result += roman_numeral[roman_string[i]] - 2 * roman_numeral[roman_string[i - 1]]
         else:
-            sumi = sumi + check_roman(roman_string[i])
-    return sumi
+            result += roman_numeral[roman_string[i]]
+    return result
