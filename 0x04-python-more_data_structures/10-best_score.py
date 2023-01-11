7f1e19bdceb2ec369926f7af9a4bafcba1b33012
Donald <donumeh54@gmail.com>
2023-01-11 10:35:56 +0100

    find the highest score in a list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == {}:
-        return (None)
+    if a_dictionary is None:
+        return "None"
     num = -10000000000000
     name = ''
     for k, v in a_dictionary.items():
