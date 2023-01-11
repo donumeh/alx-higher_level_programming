657ac2c06c723dfa003f8e2d61869ad6f1dc8a7b
Donald <donumeh54@gmail.com>
2023-01-11 10:26:35 +0100

    updated a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def update_dictionary(a_dictionary, key, value):
-    count = 0;
+    count = 0
     for i, v in a_dictionary.items():
         count = count + 1
         if i == key:
-            a_dictionary[i] = value
-        elif (i != key) and (count == len(a_dictionary)):
+            a_dictionary[key] = value
+        elif (i != key) and (count == (len(a_dictionary) - 1)):
             a_dictionary[key] = value
             break
-    return a_dictionary    
+    return a_dictionary
