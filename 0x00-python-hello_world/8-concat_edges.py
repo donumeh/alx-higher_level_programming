1d94b19f55dd989c63da4ad08d084b283f298f98
Donald <user@example.com>
2023-10-02 13:22:18 +0100

    contenates snippets together

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " " + str[107:111] + " " + str[:6]
-print(str)
+new_str = str[39:67] + str[107:112] + str[:6]
+print(f"{new_str}")
