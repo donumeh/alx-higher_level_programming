864d0d33123c6e0512314f4ddab93240620cec3c
Donald <user@example.com>
2023-10-02 13:47:53 +0100

    used sys write to send string to std err

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/bin/python3.10
 import sys
 
-sys.stderr.write("and that piece of art is useful - Dora korpar, 2015-10-19\n")
+sys.stderr.write(f"and that piece of art is useful - Dora korpar, 2015-10-19\n")
 sys.exit(1)
