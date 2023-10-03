132b64e172306b159c90536982cc4e4245ea5890
Donald <user@example.com>
2023-10-03 15:47:38 +0100

    checks the last digit if its greater or less than or 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
-new_number = number;
-if (number < 0):
+new_number = number
+if number < 0:
     new_number = -number
 
 last_digit = new_number % 10
-if (number < 0):
+if number < 0:
     last_digit = -last_digit
 print(f"Last digit of {number:d} is {last_digit:d} and is", end=" ")
 
-if (last_digit > 5):
+if last_digit > 5:
     print(f"greater than 5")
-elif (last_digit == 0):
+elif last_digit == 0:
     print(f"0")
-elif (last_digit < 6 and last_digit != 0)
+elif last_digit < 6 and last_digit != 0:
     print(f"less than 6 and not 0")
