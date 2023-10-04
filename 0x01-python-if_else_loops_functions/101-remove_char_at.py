1ba063d422a506dbc51cc9bba433c7f42ced8435
Donald <user@example.com>
2023-10-04 12:03:52 +0100

    remove a char at a position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+
+def remove_char_at(str, n):
+    new_str = ""
+
+    if (len(str) < n) or (n < 0):
+        return str
+
+    for i in str:
+        if str[n] == i:
+            continue
+        else:
+            new_str += i
+    return new_str
