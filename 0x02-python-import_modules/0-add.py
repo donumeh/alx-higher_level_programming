7365ae7fbba42f0fea0758238c0ec1621ce806d0
Donald <user@example.com>
2023-10-05 14:20:21 +0100

    prints the addition of two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,12 +2,14 @@
 
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
     result = add(a, b)
 
     print("{:d} + {:d} = {:d}".format(a, b, result))
+
 
 if __name__ == "__main__":
     main()
