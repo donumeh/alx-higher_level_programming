e18ccb1e22201d8fb9db3a8a162785f2c9b26390
Donald <user@example.com>
2023-10-05 15:52:54 +0100

    prints the function names in a module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.8.5
+
+import hidden_4
+
+def main():
+    fns = dir(hidden_4)
+
+    for i in fns:
+        if i[0:2] == "__":
+            continue
+        else:
+            print("{}".format(i))
+
+if __name__ == "__main__":
+    main()
+
