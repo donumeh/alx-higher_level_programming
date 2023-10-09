204ef702c36b3bcaf4f6d41427279b6ba03d3255
Donald <user@example.com>
2023-10-09 17:39:40 +0100

    print a reversed list of integers

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
+
+
+if __name__ == "__main__":
+    print_reversed_list_integer(my_list)
