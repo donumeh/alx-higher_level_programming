8510884d3247514cd06edd537779751277d66ada
Donald <user@example.com>
2023-10-09 18:19:05 +0100

    print a matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end="")
+            if j != i[len(i) - 1]:
+                print("", end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix)
