845db7e70bca1335ab44d5dacc24c995e4526ad3
Donald <user@example.com>
2023-10-09 18:35:32 +0100

    adds a tuple index

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_len = len(tuple_a)
+
+        for i in range(tuple_len, 2):
+            tuple_a += (0,)
+
+    if len(tuple_b) < 2:
+        tuple_len = len(tuple_b)
+
+        for i in range(tuple_len, 2):
+            tuple_b += (0,)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
