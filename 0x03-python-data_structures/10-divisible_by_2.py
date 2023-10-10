f79d3ce100d3385653f21a1d60dbd2ac277860c1
Donald <user@example.com>
2023-10-10 19:33:17 +0100

    check if data value at element is divisible by 2 or not

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     div_check = []
 
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             div_check.append(True)
         else:
             div_check.append(False)
