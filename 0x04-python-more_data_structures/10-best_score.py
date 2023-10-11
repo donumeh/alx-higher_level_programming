1f8cbb5d0303c97725ead85338bd097c74767439
Donald <user@example.com>
2023-10-11 17:43:02 +0100

    Print name of key with highest value (int)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,11 @@
 
 
 def best_score(a_dictionary):
-    name = ""
-    value = -10000
-
     if a_dictionary is None:
         return None
+
+    name = None
+    value = float("-inf")
 
     for k, v in a_dictionary.items():
         if v > value:
