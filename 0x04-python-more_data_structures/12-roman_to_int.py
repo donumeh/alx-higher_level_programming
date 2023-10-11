63b55e71432741c4a465d7fd6ca8c200091beb23
Donald <user@example.com>
2023-10-11 18:49:53 +0100

    convert roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    numerals = \
-            {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result = 0
-    prev = ""
-    current = ""
-    for char in roman_string:
-        for key, value in numerals.items():
-            current = char
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
 
-            if current == "X":
-                if prev == "I":
-                    result -= 2
-            elif current == "L":
-                if prev == "X":
-                    result -= 20
-            elif current == "C":
-                if prev == "X":
-                    result -= 20
-            elif current == "D":
-                if prev == "C":
-                    result -= 200
-            elif current == "M":
-                if prev == "C":
-                    result -= 200
-            prev = char
-            if char == key:
-                result += value
-    return result
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
+        else:
+            num += roman_dict[roman_string[i]]
+    return (num)
