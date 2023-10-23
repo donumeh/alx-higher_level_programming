abae977b2a12671dbbb9effddf385c4587d7fe70
Donald <user@example.com>
2023-10-23 16:20:12 +0100

    safe print a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0;
+    i = 0
     while i < x:
         try:
             print("{}".format(my_list[i]), end="")
@@ -11,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
             return i
     print()
     return i
-
