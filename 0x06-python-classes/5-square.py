33c57f3f6c3790fa1b89d4c451f3db7f54d9e010
Donald <user@example.com>
2023-10-25 15:29:47 +0100

    a method that draws a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+
+"""
+This modules declares a Square class
+
+Example:
+    Square = __import__('0-square').Square
+
+    my_square = Square()
+"""
+
+
+class Square:
+    """
+    This class creates Square objects to compute sqaures
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the class with field size
+
+        Args:
+            size (type: not_known): add a new size to the square
+
+        Return: None
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Computes the area of a square
+
+        Args:
+            None
+
+        Return: area of a square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Gets the value of size
+
+        Return: value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the value of size
+
+        Args:
+            value (int): param of type int to set instance attr size
+
+        Return: void
+        """
+
+        # raise TypeError if value isn't int
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        # raise ValueError if value appears to be negative
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        # else set size
+        self.__size = value
+
+    def my_print(self):
+        """
+        Prints a square
+
+        Args:
+            None
+
+        Return:
+            None
+        """
+        num = self.__size
+        if num != 0:
+            i = 0
+            while i < num:
+                print("#"*num)
+                i += 1
+        else:
+            print()
