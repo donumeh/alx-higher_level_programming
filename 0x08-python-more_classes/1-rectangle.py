9f48a2181c8dbabab3e7cf0e0435c5253b3788a0
Donald <user@example.com>
2023-10-30 17:47:10 +0100

    rectangle object created with width and height attr

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,6 +18,16 @@ class Rectangle:
         Return:
             void
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self._Rectangle__height = height
         self._Rectangle__width = width
 
