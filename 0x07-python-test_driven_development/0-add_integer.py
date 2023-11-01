18042ec6df67557a4495f5089d35dd2058084f93
Donald <user@example.com>
2023-11-01 15:53:30 +0100

    Adds two int together

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,15 +10,25 @@ Definitions:
 def add_integer(a, b=98):
     """
     This function adds two values together
+
+    Args:
+        a: first int.
+        b: second int, default value is 98
+
+    Raises:
+        TypeError: if a, b are neither int nor float
+
+    Returns:
+        sum of a and b
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
 
-    if type(a) is float:
-        a = round(a)
-    if type(b) is float:
-        b = round(b)
 
-    return a + b
+# if __name__ == "__main__":
+#    import doctest
+#    doctest.testfile("tests/0-add_integer.txt")
