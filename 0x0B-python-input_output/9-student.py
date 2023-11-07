24d0a0db112646b103bc7a9c78f4b48ee5a35332
Donald <user@example.com>
2023-11-07 23:33:56 +0100

    creates a student class to return a dictionary representation from the class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+Module that contains a class Student
+to store a student details
+"""
+
+
+class Student:
+    """
+    Class student that defines a student
+
+    Public instance attr:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): the age of the student
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Function that init the class Student
+        and creates an instance of it
+
+        Args:
+            firt_name: the first name of the student
+            last_name: the last name of the student
+            age: the age of the student
+
+        Return: Create Instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Function the retrieves a dictionary
+        representation of a Student instance
+
+        Args: None
+
+        Return: dictionary representation
+        """
+
+        return self.__dict__
