e43b30828fbe5881db2f65da687ee8117cd77850
Donald <user@example.com>
2023-11-07 22:56:10 +0100

    add an arg to a json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -22,26 +22,27 @@ def main():
     """
 
     arg_list = list()
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
+
     filename = "add_item.json"
     try:
-        arg_list = load_from_json_file(filename)
-    except:
+        arg_list = load(filename)
+    except Exception:
         pass
 
     argv_len = len(sys.argv)
 
     if argv_len <= 1:
-        save_to_json_file(arg_list, filename)
+        save(arg_list, filename)
     else:
         for i in range(argv_len):
             if i == 0:
                 continue
             arg_list.append(sys.argv[i])
-            save_to_json_file(arg_list, filename)
+            save(arg_list, filename)
     return
+
 
 if __name__ == "__main__":
     main()
