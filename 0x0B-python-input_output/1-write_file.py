9e1b623fdfeeed1e781db7a175cf27e7f3126d46
Donald <user@example.com>
2023-11-07 20:35:42 +0100

    write a text into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module that has a function that writes to a file"""
+
+def write_file(filename="", text=""):
+    """Function that writes to a file"""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        nb_chars = file.write(text)
+
+    return nb_chars
