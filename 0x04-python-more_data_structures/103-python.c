cd835c86e90288c68cdbd21b0c5128aac256ebae
Donald <user@example.com>
2024-01-11 10:39:19 +0100

    cpython code correction (inclusion of header file)

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,10 @@
 #include <Python.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
 
 /**
  * print_python_list - print a python list info
@@ -59,10 +65,14 @@ void print_python_bytes(PyObject *p)
 	printf("\n  first %ld bytes: ", maxPrint);
 	for (index = 0; index < maxPrint; index++)
 	{
+		char c = byte->ob_sval[index];
+
+		if (c < 0)
+			c = 256 + c;
 		if (index + 1 == maxPrint)
-			printf("%.2x\n", byte->ob_sval[index]);
+			printf("%2x\n", c);
 		else
-			printf("%.2x ", byte->ob_sval[index]);
+			printf("%2x ", c);
 	}
 
 }
