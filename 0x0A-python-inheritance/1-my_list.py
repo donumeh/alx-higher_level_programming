9460c1735dcd34ac3b9bb5c99de8e671f7e04e0b
Donald <user@example.com>
2024-02-05 17:55:28 +0100

    prints a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""This module contains a class that inherits from List"""
+
+class MyList(list):
+
+    """This program inherits from list and builds ontop of it"""
+
+    def print_sorted(self):
+        """Sorted the lists and prints
+
+        Parameter:
+            none
+
+        Return:
+            none
+        """
+
+        sorted_list = sorted(self, reverse=False)
+        print(sorted_list)
+
