5a04432548801f1ce298dde84587e1b3ac2db2c3
Donald <user@example.com>
2024-02-06 20:03:34 +0100

    checks computes and data

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,6 +3,22 @@
 """ Module reads from stdin and gets details from it"""
 
 
+def print_metrics(file_size, status_num):
+    """Function that prints metric based on parameter
+
+    Prarameter:
+        file_size (int): the size of a file
+        status_count (dict): a dict to print out
+
+    Return:
+        None
+    """
+    print("File size: {}".format(file_size))
+    for status_code in sorted(status_num.keys()):
+        print("{}: {}".format(status_code, status_num[status_code]))
+
+
+
 def log_parser():
     """
     Function reads computes metrics and get status code from it
@@ -17,27 +33,29 @@ def log_parser():
 
     file_size = 0
     counter = 0
-    status_num = {200: 0, 301: 0, 400: 0, 401: 0, 404: 0, 405: 0, 500: 0}
+    status_num = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    for line in sys.stdin:
-        status_code = int(line.split(" ")[-2])
-        file_size += int(line.split(" ")[-1])
 
-        if status_code in status_num:
-            status_num[status_code] += 1
+    try:
+        
+        for line in sys.stdin:
+            try:
+                parts = line.split()
+                status_code = int(parts[-2])
+                file_size += int(parts[-1])
+                status_num[status_code] += 1
+                counter += 1
 
-        if counter == 10:
-            print("File size: {}".format(file_size))
 
-            for key, value in status_num.items():
-                if value <= 0:
-                    continue
-                print("{}: {}".format(key, value))
-                status_num[key] = 0
+                if counter % 10 == 0:
+                    print_metrics(file_size, status_num)
+            except (ValueError, IndexError):
+                continue
 
-            counter = 0
+    except KeyboardInterrupt as e:
+        print_metrics(file_size, status_num)
+        print(e)
 
-        counter += 1
 
 
 if __name__ == "__main__":
