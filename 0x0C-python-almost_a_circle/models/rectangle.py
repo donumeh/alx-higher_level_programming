df0b7d38e7ae2cc8982204d4d303b8b8e137a5d4
Donald <user@example.com>
2024-02-12 22:28:33 +0100

    readmes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,8 @@ from models.base import Base
 
 """
 Modules has a class that inherits from Base
+
+
 """
 
 
@@ -11,11 +13,15 @@ class Rectangle(Base):
 
     """
     This Class creates a rectangle object using the base class
+
+
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Function initiates the class module Rectangle
+
+
         """
         super().__init__(id)
         self.__width = width
@@ -27,40 +33,56 @@ class Rectangle(Base):
     def width(self):
         """
         Gets the width
+
+
         """
         return self.__width
 
     @width.setter
     def width(self, width):
-        """Sets the width"""
+        """Sets the width
+
+        """
         self.__width = width
 
     @property
     def height(self):
-        """Gets the height"""
+        """Gets the height
+
+        """
         return self.__height
 
     @height.setter
     def height(self, height):
-        """Sets the height"""
+        """Sets the height
+
+        """
         self.__height = height
 
     @property
     def x(self):
-        """Gets x"""
+        """Gets x
+
+        """
         return self.__x
 
     @x.setter
     def x(self, x):
-        """Sets x"""
+        """Sets x
+        
+        """
         self.__x = x
 
     @property
     def y(self):
-        """Get y"""
+        """Get y
+
+        """
         return self.__y
 
     @y.setter
     def y(self, y):
-        """Set y"""
+        """Set y
+
+        """
         self.__y = y
