7446305130b729481e411c225e05ac613b79d744
Donald <donumeh54@gmail.com>
2024-05-24 13:11:00 +0100

    converts database charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,12 +2,12 @@
 -- collate utf8mbt_unicode_ci
 
 ALTER DATABASE hbtn_0c_0
-	CHARACTER SET utf8mb4 
-	COLLATE utf8mb4_unicode_ci;
+	CHARACTER SET = utf8mb4 
+	COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
+ALTER TABLE first_table
 	CONVERT TO CHARACTER SET utf8mb4 
 	COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
+ALTER TABLE first_table
 	MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
