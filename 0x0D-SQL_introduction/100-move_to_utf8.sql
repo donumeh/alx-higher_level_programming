411780b88aa2fa9e6eb2e983a95144f7e44a131a
Donald <donumeh54@gmail.com>
2024-05-24 13:18:20 +0100

    converts database charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -12,5 +12,5 @@ ALTER TABLE first_table
 	COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-	MODIFY name VARCHAR(256)
+	MODIFY name VARCHAR(256) CHARACTER SET ''
 	COLLATE utf8mb4_unicode_ci;
