d3e6de02aa33ea2f370097d67e5a28b148efb86e
Donald <donumeh54@gmail.com>
2024-05-25 14:12:05 +0100

    shows all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,18 @@
+-- Script that lists all shows contained in the database
+
+SELECT
+	tv_shows.title AS title,
+	tv_show_genres.genre_id AS genre_id
+FROM
+	tv_shows
+LEFT JOIN
+	tv_show_genres
+ON
+	tv_shows.id = tv_show_genres.show_id
+LEFT JOIN
+	tv_genres
+ON
+	tv_genres.id = tv_show_genres.genre_id
+ORDER BY
+	tv_shows.title ASC,
+	tv_show_genres.genre_id ASC;
