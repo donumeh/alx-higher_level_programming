e9f958cd38f0498d0e9d53317da733ec49283d8b
Donald <donumeh54@gmail.com>
2024-05-25 12:48:35 +0100

    create a new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Script that creates a user in Mysql
 
--- UNINSTALL COMPONENT 'validate_password';
+
+UNINSTALL COMPONENT 'validate_password';
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
