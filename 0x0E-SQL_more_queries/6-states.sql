c59a9c96b7195e47c7ddb1418baa5e2a60935ddf
Donald <donumeh54@gmail.com>
2024-05-25 13:17:11 +0100

    create a database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Script that creates a database `hbtn_0d_usa` and table `states`
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
+);
