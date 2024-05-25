0b6ec542e3126ce7a7d0d3a070672735d3e34927
Donald <donumeh54@gmail.com>
2024-05-25 13:39:42 +0100

    list all cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,14 @@
 -- Script that list all cities in a database
 
-SELECT cities.id AS id,
-		cities.name AS name,
-		states.name AS name
-	FROM cities JOIN states
-		ON states.id = cities.states_id
-	ORDER BY cities.id ASC;
+SELECT
+	cities.id AS id,
+	cities.name AS name,
+	states.name AS name
+FROM 
+	cities 
+JOIN
+	states
+ON
+	states.id = cities.state_id
+ORDER BY
+	cities.id ASC;
