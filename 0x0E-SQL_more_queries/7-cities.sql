1be7a33d36082407c54f6315c1425916e24534c0
Donald <donumeh54@gmail.com>
2024-05-25 13:24:38 +0100

    created a new table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	PRIMARY KEY(id)),
