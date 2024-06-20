68d851f9fbb3008440886706e0032b347ce5ff40
Donald <donumeh54@gmail.com>
2024-06-20 02:29:44 +0100

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -9,16 +9,14 @@ let arg = process.argv[2];
 arg = parseInt(arg);
 
 if (arg) {
-	if (arg > 0) {
-
-		for (let i = 0; i < arg; i++) {
-			for (let y = 0; y < arg; y++) {
-				process.stdout.write('X');
-			}
-			process.stdout.write('\n');
-		}
-	}
-}
-else {
-	console.log('Missing size');
+  if (arg > 0) {
+    for (let i = 0; i < arg; i++) {
+      for (let y = 0; y < arg; y++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+} else {
+  console.log('Missing size');
 }
