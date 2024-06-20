53a18698630538bae90b2558dbf04660be31b48e
Donald <donumeh54@gmail.com>
2024-06-20 01:35:51 +0100

    print some string literals

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/**
+ * Saves some constant variables
+ * and prints them using console.log()
+ */
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
