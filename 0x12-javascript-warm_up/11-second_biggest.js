42b5ef0b63138cf6b6e96692323b74e2072dd432
Donald <donumeh54@gmail.com>
2024-06-20 03:11:36 +0100

    prints the second highest in the list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@
  * prints the second largest number
  */
 
-let args = process.argv.slice(2).map(Number);
+const args = process.argv.slice(2).map(Number);
 
 if (args.length === 0 || args.length === 1) {
-	console.log(0);
+  console.log(0);
 } else {
-	let max = Math.max(...args);
-	let secondBiggest = Math.max(...args.filter(x => x < max));
-	console.log(secondBiggest);
+  const max = Math.max(...args);
+  const secondBiggest = Math.max(...args.filter(x => x < max));
+  console.log(secondBiggest);
 }
