36e5c9aa510d0302171977edf791e5a15b2131c8
Donald <donumeh54@gmail.com>
2024-06-20 02:10:55 +0100

    prints 3 lines but using an array of string

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * prints 3 lines but by using an array of string and a loop
+ */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const ar of array) {
+  console.log(ar);
+}
