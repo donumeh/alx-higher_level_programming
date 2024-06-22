fd8a330306cbe832d6f4b173d217da735f03d50d
Donald <donumeh54@gmail.com>
2024-06-22 03:11:18 +0100

    defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * converts a number from base 10 to another base
+ */
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
