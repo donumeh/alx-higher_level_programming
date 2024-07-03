544723459a26b912f28f7832e0b7d0c74451b3c5
Donald <donumeh54@gmail.com>
2024-07-03 02:21:06 +0100

    python file that contains city class and print out

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,6 +8,7 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class City(Base):
 
     """
@@ -19,5 +20,3 @@ class City(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-
-
