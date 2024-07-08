c2ea022c4d3665a7d6838a261edc6b8cd9e3a609
Donald <donumeh54@gmail.com>
2024-07-08 17:37:36 +0100

    script that takes in a URL, sends a req and displays the size of the req

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Request a url and gets size
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-# Send a GET request to the URL and get the size of the body in bytes
-SIZE=$(curl -sI "$URL" | grep -i content-length | awk '{print $2}' | tr -d '\r')
-
-echo "$SIZE"
-
+[ $# -eq 1 ] && curl -sI "$1" | grep -i Content-Length | awk '{print $2}' || echo "Usage: $0 <URL>"
