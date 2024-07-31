444efe664e6e9d6d9651465fe60fc03d65e538d3
Donald <donumeh54@gmail.com>
2024-07-31 11:45:26 +0100

    bash script that takes in a URL and send a GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL and displays the body of the response
-[[ $# -eq 1 ]] && [[ $(curl -sI "$1" | head -n 1 | tr ' ' '\n' | tail -n 2 | head -n 1) -eq "200" ]] && curl -sg "$1"
+# Bash script that takes in usr and uses get to request to the URL
+[[ $# -eq 1 ]] && url="$1" && while [[ $(curl -sI -o /dev/null -w "%{http_code}" "$url") =~ ^3 ]]; do url=$(curl -sI "$url" | grep -i "location:" | awk '{print $2}' | tr -d '\r'); done && [[ $(curl -sI -o /dev/null -w "%{http_code}" "$url") == "200" ]] && curl -sg "$url"
