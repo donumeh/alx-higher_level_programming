00937ea6c58ade9dc594481b28283b16c3470259
Donald <donumeh54@gmail.com>
2024-07-31 11:19:10 +0100

    bash script that takes in a URL and send a GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the body of the response
-if [[ $# -eq 1 ]]
-then
-	status_code=$(curl -sI "$1" | head -n 1 | tr ' ' '\n' | tail -n 2 | head -n 1)
-	
-	if [[ $status_code -eq "200" ]]
-	then
-		curl -sg "$1"
-		exit
-	else
-		exit 1
-	fi
-fi
+[[ $# -eq 1 ]] && [[ $(curl -sI "$1" | head -n 1 | tr ' ' '\n' | tail -n 2 | head -n 1) -eq "200" ]] && curl -sg "$1"
