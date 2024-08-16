c52a2e09d439c1740a18c8f0b4813a86e7078f35
Donald <donumeh54@gmail.com>
2024-08-17 00:46:09 +0100

    Takes in a letter a send a POSt req to it

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST req to rul
+"""
+
+import requests
+import sys
+
+
+def main():
+    """
+    Takes a letter and sends a POST req
+    """
+
+    if len(sys.argv) < 2:
+        param = ""
+    else:
+        param = sys.argv[1]
+
+    data = {"q": param}
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        json_dict = res.json()
+
+        if len(json_dict) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_dict["id"], json_dict["name"]))
+    except:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
