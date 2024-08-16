bc8aa6a68537c09693dc78f42f35894c4bbdc3e4
Donald <donumeh54@gmail.com>
2024-08-16 01:16:36 +0100

    takes in a url and sends a get request to the url

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in usr and uses get to request to the URL
-[[ $# -eq 1 ]] && url="$1" && while [[ $(curl -sI -o /dev/null -w "%{http_code}" "$url") =~ ^3 ]]; do url=$(curl -sI "$url" | grep -i "location:" | awk '{print $2}' | tr -d '\r'); done && [[ $(curl -sI -o /dev/null -w "%{http_code}" "$url") == "200" ]] && curl -sg "$url"
+[[ -eq 1 ]] && curl -L -X GET "$1"
